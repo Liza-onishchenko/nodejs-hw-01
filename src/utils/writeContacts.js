fe9c48c88c878b89ce65d0a42103af5c44cc51d6
Liza <onishenkolizka99@gmail.com>
2024-12-01 11:38:31 +0200

    hw-01

---
+++ src/utils/writeContacts.js
@@ -0,0 +1,7 @@
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+
+export const writeContacts = async (contacts) => {
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+};
+//updatedContacts
